e346221211f3c10c69d82640c8a7fa5e5f66e3f2
l <109516388+Hamish2022@users.noreply.github.com>
2023-11-25 17:42:38 +1100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,623 @@
+var rank = "leader";
+onEvent("button4", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button8", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button9", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button12", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button13", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button10", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button11", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button15", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button17", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button18", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button19", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button20", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button21", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button24", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button25", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button22", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button23", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button27", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button28", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button30", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button31", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button32", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button33", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button34", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button35", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button36", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button37", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button39", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button40", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button41", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button43", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button44", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button45", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button48", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button49", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button46", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button47", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button51", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button52", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button53", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button54", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button56", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button57", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button60", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button61", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button58", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button59", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button63", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button64", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button65", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button66", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button67", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button69", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button72", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button73", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button70", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button71", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button75", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button76", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button77", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button78", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button79", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button80", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button84", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button85", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button82", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button83", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button87", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button88", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button89", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button90", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button91", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button92", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button93", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button97", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button94", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button95", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button99", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button100", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button101", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button102", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button103", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button104", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button105", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button108", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button106", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button107", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button99", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button100", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button101", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button102", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button103", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button104", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button105", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button108", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button106", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button107", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button111", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button112", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button113", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button114", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button115", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button116", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button117", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button120", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button121", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button119", "click", function( ) {
+  setScreen("task");
+});
+// 
+onEvent("button123", "click", function( ) {
+  setScreen("AI");
+});
+onEvent("button124", "click", function( ) {
+  setScreen("data+");
+});
+onEvent("button125", "click", function( ) {
+  setScreen("leadertasks");
+});
+onEvent("button126", "click", function( ) {
+  setScreen("pledge");
+});
+onEvent("button127", "click", function( ) {
+  setScreen("emails");
+});
+onEvent("button128", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button129", "click", function( ) {
+  setScreen("announced");
+});
+onEvent("button132", "click", function( ) {
+  setScreen("chat");
+});
+onEvent("button133", "click", function( ) {
+  setScreen("ranks");
+});
+onEvent("button130", "click", function( ) {
+  setScreen("announce");
+});
+onEvent("button139", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button138", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button144", "click", function( ) {
+  setScreen("members");
+});
+onEvent("button142", "click", function( ) {
+  setScreen("members");
+});
+var name = "";
+onEvent("button1", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if ((records[i]).username == getText("text_input1")) {
+        if ((records[i]).password == getText("text_input2")) {
+          rank = (records[i]).rank;
+          name = (records[i]).username;
+          setScreen("AI");
+        }
+      }
+    }
+  });
+});
+timedLoop(5000, function() {
+  setText("text_area17", "");
+  readRecords("chat", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      setText("text_area17", getText("text_area17") + ("\n" + ("\n" + (records[i]).chat)));
+    }
+  });
+});
+onEvent("button137", "click", function( ) {
+  createRecord("chat", {chat:(name + (": " + getText("text_input6")))});
+});
+onEvent("button134", "click", function( ) {
+  createRecord("DATA(confidential)", {name:(getText("text_input4")), discription:(getText("text_area2")), rank:"public"});
+});
+var Q = "";
+onEvent("text_input3", "change", function( ) {
+  Q = getText("text_input3");
+  setText("label6", "your question: " + Q);
+  if (Q == "hi" || (Q == "hello" || (Q == "Hi" || Q == "Hello"))) {
+    setText("text_area1", "bots response: Hi I am the CCI data bot");
+    setText("text_input3", "");
+  } else if ((Q == "who is the leader")) {
+    setText("text_area1", "bots response: sorry this data is confidential");
+    setText("text_input3", "");
+  } else if ((Q == "what does the CCI stand for" || Q == "what does the cci stand for")) {
+    setText("text_area1", "bots response: Childrens Central Intelligence");
+    setText("text_input3", "");
+  } else if ((Q == "what if i break the pledge")) {
+    setText("text_area1", "bots response: you will let down the entire CCI and they will blacklist you");
+    setText("text_input3", "");
+  } else if ((Q == "make data up")) {
+    if (rank == "leader") {
+      showElement("button147");
+      showElement("text_input14");
+      showElement("text_input15");
+      setText("text_input3", "");
+    }
+  } else {
+    readRecords("DATA(confidential)", {}, function(records) {
+      for (var i =0; i < records.length; i++) {
+        if ((records[i]).name == getText("text_input3")) {
+          if ((records[i]).rank == "public") {
+            setText("text_area1", (records[i]).name + ("'s data is" + (records[i]).discription));
+          } else if (((records[i]).rank == "upper")) {
+            if (rank == "leader" || rank == "OP") {
+              setText("text_area1", (records[i]).name + ("'s data is " + (records[i]).discription));
+            } else {
+              setText("text_area1", "bots response: sorry this data is confidential");
+            }
+          }
+        }
+      }
+    });
+  }
+  setText("text_input3", "");
+});
+onEvent("button147", "click", function( ) {
+  readRecords("DATA(confidential)", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if ((records[i]).name == getText("text_input14")) {
+        if (getText("text_input15") == "upper") {
+          updateRecord("DATA(confidential)", {id:(i + 2), rank:'upper', name:(records[i]).name, description:((records[i]).description)});
+          setText("text_area1", "bots response: " + ((records[i]).name + " is now upper rank"));
+        }
+        if (getText("text_input15") == "public") {
+          updateRecord("DATA(confidential)", {id:(i + 2), rank:'public', name:(records[i]).name, description:((records[i]).description)});
+          setText("text_area1", "bots response: " + ((records[i]).name + " is now public rank"));
+        }
+      }
+    }
+  });
+});
+timedLoop(10000, function() {
+  setText("text_area13", "");
+  readRecords("accounts", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      setText("text_area13", getText("text_area13") + ("\n" + ("\n" + (records[i]).username)));
+    }
+  });
+});
+onEvent("button145", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    if (rank == "leader" || rank == "OP") {
+      for (var i =0; i < records.length; i++) {
+        if ((records[i]).username == getText("text_input13")) {
+          updateRecord("accounts", {id:(i + 9), rank:'banned', username:((records[i]).username), password:((records[i]).password)});
+        }
+      }
+    }
+  });
+});
+onEvent("button146", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    if (rank == "leader" || rank == "OP") {
+      for (var i =0; i < records.length; i++) {
+        if ((records[i]).username == getText("text_input13")) {
+          updateRecord("accounts", {id:(i + 9), rank:'agent', username:((records[i]).username), password:((records[i]).password)});
+        }
+      }
+    }
+  });
+});
+onEvent("button151", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if ((records[i]).username == getText("text_input13")) {
+        setText("label18", (records[i]).rank);
+      }
+    }
+  });
+});
+onEvent("button149", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    if (rank == "leader") {
+      for (var i =0; i < records.length; i++) {
+        if ((records[i]).username == getText("text_input13")) {
+          if ((records[i]).rank == "OP") {
+            updateRecord("accounts", {id:(i + 9), rank:'leader', username:((records[i]).username), password:((records[i]).password)});
+          }
+          if (records[i].rank == "PRIORagent") {
+            updateRecord("accounts", {id:(i + 9), rank:'OP', username:((records[i]).username), password:((records[i]).password)});
+          }
+          if (records[i].rank == "agent") {
+            updateRecord("accounts", {id:(i + 9), rank:'PRIORagent', username:((records[i]).username), password:((records[i]).password)});
+          }
+        }
+      }
+    }
+  });
+});
+onEvent("button148", "click", function( ) {
+  readRecords("accounts", {}, function(records) {
+    if (rank == "leader") {
+      for (var i =0; i < records.length; i++) {
+        if ((records[i]).username == getText("text_input13")) {
+          if (records[i].rank == "PRIORagent") {
+            updateRecord("accounts", {id:(i + 9), rank:'agent', username:((records[i]).username), password:((records[i]).password)});
+          }
+          if ((records[i]).rank == "OP") {
+            updateRecord("accounts", {id:(i + 9), rank:'PRIORagent', username:((records[i]).username), password:((records[i]).password)});
+          }
+          if ((records[i]).rank == "leader") {
+            if (records[i].username != "leader") {
+              updateRecord("accounts", {id:(i + 9), rank:'OP', username:((records[i]).username), password:((records[i]).password)});
+            }
+          }
+        }
+      }
+    }
+  });
+});
+var x = true;
+onEvent("button143", "click", function( ) {
+  x = true;
+  readRecords("accounts", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if ((records[i]).username == getText("text_input10")) {
+        x = false;
+        break;
+      }
+    }
+    if (rank == "leader") {
+      if (x) {
+        createRecord("accounts", {username:(getText("text_input10")), password:(getText("text_input11")), rank:(getText("text_input12"))});
+      }
+    }
+  });
+});
+onEvent("button152", "click", function( ) {
+  setText("text_area5", "");
+  readRecords("emails", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if (records[i].to == name) {
+        setText("text_area5", getText("text_area5") + ("\n" + ("\n" + ((records[i]).to + (" " + (records[i]).message)))));
+      }
+    }
+  });
+});
+onEvent("button136", "click", function( ) {
+  createRecord("emails", {to:(getText("text_input5")), message:(getText("text_area12"))});
+});
+onEvent("button153", "click", function( ) {
+  readRecords("A", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if (i == 1) {
+        setText("text_area14", (records[i]).a);
+      }
+      if (i == 2) {
+        setText("text_area15", (records[i]).a);
+      }
+      if (i == 3) {
+        setText("text_area16", (records[i]).a);
+      }
+    }
+  });
+});
+onEvent("button140", "click", function( ) {
+  if (rank == "leader") {
+    updateRecord("A", {id:(getNumber("text_input7") + 1), a:(getText("text_area19"))});
+  }
+});
+onEvent("button135", "click", function( ) {
+  setText("text_area3", "");
+  readRecords("tasks", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if (records[i].who == name) {
+        setText("text_area3", getText("text_area3") + ("\n" + ("\n" + ((records[i]).who + (" " + (records[i]).task)))));
+      }
+    }
+  });
+});
+onEvent("button141", "click", function( ) {
+  if (rank == "leader" || rank == "OP") {
+    createRecord("tasks", {who:(getText("text_input9")), task:(getText("text_area20"))});
+  }
+});
+onEvent("button154", "click", function( ) {
+  readRecords("tasks", {}, function(records) {
+    for (var i =0; i < records.length; i++) {
+      if ((records[i]).who == getText("text_input8")) {
+        deleteRecord("tasks", {id:i + 1});
+      }
+    }
+  });
+});
